4fe30f3e23bb9a3d13eae3b3a4406b240fa12475
Yor <freyorla28@gmail.com>
2024-09-27 09:55:02 +0700

    Update perkalian.cpp

--- Pertemuan4/Soal2/perkalian.cpp
+++ Pertemuan4/Soal2/perkalian.cpp
@@ -13,3 +13,29 @@ int main(){
     }
 return 0;
 }
+
+int main() {
+    ifstream testFile("test.txt");
+    int input;
+    string expected_output;
+
+    if (!testFile) {
+        cerr << "Error: Could not open test.txt" << endl;
+        return 1;
+    }
+
+    int test_num = 1;
+    while (testFile >> input && getline(testFile >> ws, expected_output)) {
+        string output = perkalian(input);
+        if (output == expected_output) {
+            cout << "Test " << test_num << " passed!" << endl;
+        } else {
+            cout << "Test " << test_num << " failed. Expected: \"" 
+                      << expected_output << "\", Got: \"" << output << "\"" << endl;
+        }
+        test_num++;
+    }
+
+    testFile.close();
+    return 0;
+}
