46cbb8d62e618d8f29a4cf04ae338301424897d5
Yor <freyorla28@gmail.com>
2024-11-26 14:09:48 +0700

    Update balikString.cpp

--- responsi/soal2/balikString.cpp
+++ responsi/soal2/balikString.cpp
@@ -4,7 +4,8 @@ using namespace std;
 
 int main(){
     string s;
-    s = "Freta Yordinia Laura";
+    getline(cin, s);
+    //s = "Freta Yordinia Laura";
     
     cout<<"Reversed kalimatnya adalah: ";
     for (int i=s.size(); i>=0; i--){
