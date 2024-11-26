973dea49a6521a511ce13db509b0d98f4992db21
Yor <freyorla28@gmail.com>
2024-11-26 14:01:44 +0700

    Update gabungData.cpp

--- responsi/soal1/gabungData.cpp
+++ responsi/soal1/gabungData.cpp
@@ -5,16 +5,25 @@ void gabungData(const int* list1, const int* list2, int size1, int size2, int* m
     int i = 0, j = 0, k = 0;
 
     while(i<size1 && j<size2){
-
+        for (int i = 0; i<size1; ++i){
+            if (size1<=size2){
+                mergeList[k++] = list1[i++];
+            }
+            else{
+                mergeList[k++] = list2[j++];
+            }
+        }
+            
     }
     while(i<size1){
-        mergeList[k++] = list1[i++]
+        mergeList[k++] = list1[i++];
     }
     while(j<size2){
-        mergeList[k++] = list2[j++]
+        mergeList[k++] = list2[j++];
     }
 }
 int main(){
+    
     const int list1 = {1, 3, 5, 7};
     const int list2 = {2, 4, 6, 8};
     const int size1 = sizeof(list1) / sizeof(list1[0]);
